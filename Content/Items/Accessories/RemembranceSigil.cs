c6f182111e62bf09cc20a4dfbeca49afe88b6b6e
MrFrozenG <danka2497@gmail.com>
2024-04-16 20:54:02 +0400

    0.0.1

---
+++ Content/Items/Accessories/RemembranceSigil.cs
@@ -0,0 +1,16 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Wos.Content.Items.Accessories;
+
+public class RemembranceSigil : ModItem
+{
+	public override void SetStaticDefaults()
+	{
+	}
+	
+	public override void SetDefaults()
+	{
+		
+	}
+}
